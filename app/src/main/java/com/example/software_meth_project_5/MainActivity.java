e67508d7e88845fd6cc33f0eca66e0beddd969de
teddy7201 <henry10004@gmail.com>
2025-04-23 22:03:58 -0400

    Views mostly done I think, missing combo views because I'm not too sure how I want the program to run yet and because of that the main screen may have to change as well soon.

--- app/src/main/java/com/example/software_meth_project_5/MainActivity.java
+++ app/src/main/java/com/example/software_meth_project_5/MainActivity.java
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.current_order_view);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.currentOrderView), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
